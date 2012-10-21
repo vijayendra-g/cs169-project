8c0558cba9e33a9fe69d95f0852a5febbaf70e6b
mwo <womengyi@gmail.com>
2012-10-21 16:48:20 -0700

    moved parse to helper

--- app/controllers/pubmed_controller.rb
+++ app/controllers/pubmed_controller.rb
@@ -1,14 +1,16 @@
 class PubmedController < ApplicationController
+  include PubmedHelper
   def index
     render 'search'
   end
 
   def search
-    @results = PubmedSearch.search(params[:search_term])
-    @pmids = @results.pmids
-    puts @pmids.length
-    puts @pmids[1]
-    render 'mesh'
+    search_results = PubmedSearch.search(params[:search_term])
+    @articleIDList = search_results.pmids
+    @results = ArticleXMLParser.new(@articleIDList) #names should be changed
+
   end
 
 end
+
+
