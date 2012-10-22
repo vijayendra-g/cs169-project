4822ab1ac1adcb4ba0b0387f020c11b87dbd8b25
mwo <womengyi@gmail.com>
2012-10-21 17:02:44 -0700

    changed query

--- app/controllers/pubmed_controller.rb
+++ app/controllers/pubmed_controller.rb
@@ -5,8 +5,11 @@ class PubmedController < ApplicationController
   end
 
   def search
+    search_query = "(((#(params[:major_topic])[Mesh Major Topic]) AND #(params[:subheading])[MeSH Subheading]) AND #(params[:terms])[MeSH Terms]) AND #(params[:search_term])"
+    puts search_query
     search_results = PubmedSearch.search(params[:search_term])
     @articleIDList = search_results.pmids
+    puts @articleIDList[0]
     @results = ArticleXMLParser.new(@articleIDList) #names should be changed
 
   end
