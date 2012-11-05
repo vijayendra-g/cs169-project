20be92073e42517e462d4626cf9dc570d8bef265
Pedro Tanaka <pedrinhotanaka@gmail.com>
2012-11-04 16:34:42 -0800

    Default is now dependent on a variable

--- app/controllers/pubmed_controller.rb
+++ app/controllers/pubmed_controller.rb
@@ -30,23 +30,26 @@ class PubmedController < ApplicationController
 
     puts "Starting final"
     if search_query != nil && params[:search_term] != nil && params[:search_term] != ""
-      search_query = "((" + search_query + ") " + connector + " Anesthesia[MeSH Terms]) " + connector + " #{params[:search_term]}"
+      search_query = "((" + search_query + ") " + connector + " " + medical_field + "[MeSH Terms]) " + connector + " #{params[:search_term]}"
     elsif search_query == nil && params[:search_term] != nil && params[:search_term] != ""
-      search_query = "(Anesthesia[MeSH Terms]) " + connector + " #{params[:search_term]}"
+      search_query = "(" + medical_field + "[MeSH Terms]) " + connector + " #{params[:search_term]}"
     elsif search_query != nil && (params[:search_term] == nil || params[:search_term] == "")
-      search_query = "(" + search_query + ") " + connector + " Anesthesia[MeSH Terms]"
+      search_query = "(" + search_query + ") " + connector + " " + medical_field +"[MeSH Terms]"
     else
-      search_query = "Anesthesia[MeSH Terms]"
+      search_query = medical_field + "[MeSH Terms]"
     end
     puts "Search query is " + search_query
     search_results = PubmedSearch.search(search_query)
     @articleIDList = search_results.pmids
     @results = ArticleXMLParser.new(@articleIDList) #names should be changed
     @current_results = Array.new
+    i = 0
     @results.each do |result|
+      break if i == 40
       @current_results.push(result)
+      i += 1
     end
-    @current_results = Kaminari::PaginatableArray.new(@current_results).page(0).per(10)
+    @current_results = Kaminari.paginate_array(@current_results).page(0).per(10)
     #i = 0
     #@results.each do |result|
     #    break if i > 5
