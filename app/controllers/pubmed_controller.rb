544316f4ed66193df52ea1df045521483fcdbc40
mwo <womengyi@gmail.com>
2012-11-11 16:30:23 -0800

    removed meshterms in view and controller, only keeping search term

--- app/controllers/pubmed_controller.rb
+++ app/controllers/pubmed_controller.rb
@@ -6,41 +6,13 @@ class PubmedController < ApplicationController
   end
 
   def search
-    if params[:major_topic] == nil && params[:subheading] == nil && params[:terms] == nil && params[:search_term] == nil
+    if params[:search_term].blank?
 	render 'search' and return
     end
-    connector = "AND"
-    #puts "Starting topic"
+    connector = 'AND'
     medical_field = "Anesthesia"
-
-    if params[:major_topic] != nil && params[:major_topic] != ""
-      search_query = "#{params[:major_topic]}[MeSH Major Topic]"
-    end
-
-    #puts "Starting subheading"
-    if params[:subheading] != nil && params[:subheading] != "" && search_query != nil
-      search_query = "(" + search_query + ") " + connector + " #{params[:subheading]}[MeSH Subheading]"
-    elsif params[:subheading] != nil && params[:subheading] != ""
-      search_query = "#{params[:subheading]}[MeSH Subheading]"
-    end
-
-    #puts "Starting terms"
-    if params[:terms] != nil && params[:terms] != "" && search_query != nil
-      search_query = "(" + search_query + ") " + connector + " #{params[:terms]}[MeSH Terms]"
-    elsif params[:terms] != nil && params[:terms] != ""
-      search_query = "#{params[:terms]}[MeSH Terms]"
-    end
-
-    #puts "Starting final"
-    if search_query != nil && params[:search_term] != nil && params[:search_term] != ""
-      search_query = "((" + search_query + ") " + connector + " " + medical_field + "[MeSH Terms]) " + connector + " #{params[:search_term]}"
-    elsif search_query == nil && params[:search_term] != nil && params[:search_term] != ""
-      search_query = "(" + medical_field + "[MeSH Terms]) " + connector + " #{params[:search_term]}"
-    elsif search_query != nil && (params[:search_term] == nil || params[:search_term] == "")
-      search_query = "(" + search_query + ") " + connector + " " + medical_field +"[MeSH Terms]"
-    else
-      search_query = medical_field + "[MeSH Terms]"
-    end
+    search_query = "(" + params[:search_term] + ") " + connector + " " + medical_field +"[MeSH Terms]"
+    
     #puts "Search query is " + search_query
     search_results = PubmedSearch.search(search_query)
     @articleIDList = search_results.pmids
@@ -60,10 +32,8 @@ class PubmedController < ApplicationController
     #    i = i + 1
     #end
     #puts "We have " + i.to_s + " articles"
-    session[:subheading] = params[:subheading]
-    session[:terms] = params[:terms]
     session[:search_term] = params[:search_term]
-    session[:major_topic] = params[:major_topic]
+
     render 'results'
   end
 
