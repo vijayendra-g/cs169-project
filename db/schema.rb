3f0eb7fec1e3dfe5973957534a1155dd2bd79a1e
mwo <womengyi@gmail.com>
2012-11-18 21:23:39 -0800

    added save article

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,26 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121118062437) do
+ActiveRecord::Schema.define(:version => 20121118223055) do
+
+  create_table "articles", :force => true do |t|
+    t.integer  "pubmed_id"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "identities", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "saved_articles", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "article_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
