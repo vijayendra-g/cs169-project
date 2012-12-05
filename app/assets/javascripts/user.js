e962182f8a8636a8ad392edebf171e0ea26bdf35
saasbook <saasbook@saasbook.(none)>
2012-12-04 21:59:53 -0800

    changed live to click again in user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,13 +1,14 @@
 $().ready(function() {
-    $(".delete-article").live(function(e) {
+    $(".delete-article").click(function(e) {
     e.preventDefault();
+    e.stopPropagation();
     if (confirm('Do you really want to delete this article?')) {
       var article_id = $(this).attr('article_id');
       var id = $(this).attr('id');
       $.ajax({
         type: "POST",
         url: 'delete_article/',
-	data: {article_id: article_id, js: 1},
+				data: {article_id: article_id, js: 1},
         success: function() {
 		    document.getElementById(id).parentNode.parentNode.removeChild(document.getElementById(id).parentNode);
             }
@@ -15,8 +16,9 @@ $().ready(function() {
     }
 
     });
-    $('.save-article,.unsave-article').live('click', function(e) {
+    $('.save-article,.unsave-article').click('click', function(e) {
       e.preventDefault();
+      e.stopPropagation();
       var article_id = $(this).attr('article_id');
       var article_title = $(this).attr('article_title');
       var id = $(this).attr('id');
@@ -26,10 +28,10 @@ $().ready(function() {
       $.ajax({
         type: "POST",
         url: '/user/save_article/',
-	data: {article_title: article_title, article_id: article_id, js: 1},
+				data: {article_title: article_title, article_id: article_id, js: 1},
         success: function() {
-		element.toggleClass('unsave-article save-article');
-		element.html('Unsave Article');
+				element.toggleClass('unsave-article save-article');
+				element.html('Unsave Article');
             }
       });
       } else if (element.hasClass('unsave-article')){
@@ -40,10 +42,10 @@ $().ready(function() {
       	$.ajax({
        	 type: "POST",
        	 url: '/user/unsave_article/',
-		data: {article_id: article_id, js: 1},
+				 data: {article_id: article_id, js: 1},
        	 success: function() {
             element.toggleClass('unsave-article save-article');
-	    element.html('Save Article');
+	    			element.html('Save Article');
             }
       	});
 	}
